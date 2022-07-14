84f312011a8e5da2d8e59b1793d9437320a613de
anucha.ph <anucha.ph@linecorp.com>
2022-07-14 19:55:11 +0700

    refactor: remove useless logic

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-	"strings"
 )
 
 func main() {
@@ -16,26 +15,10 @@ func main() {
 	sd, _ := base64.StdEncoding.DecodeString(secret)
 
 	text := string(sd)
-	splitColons := strings.Split(text, ":")
 
-	for i, j := 0, len(splitColons)-1; i < j; i, j = i+1, j-1 {
-		splitColons[i], splitColons[j] = Reverse(splitColons[j]), Reverse(splitColons[i])
-	}
-
-	for i, v := range splitColons {
-		if i == 0 {
-			whatIsIt = fmt.Sprintf("%s", v)
-		} else {
-			whatIsIt = fmt.Sprintf("%s:%s", whatIsIt, v)
-		}
+	for _, v := range text {
+		whatIsIt = fmt.Sprintf("%s%s", string(v), whatIsIt)
 	}
 
 	fmt.Println(whatIsIt)
-}
-
-func Reverse(s string) (result string) {
-	for _, v := range s {
-		result = fmt.Sprintf("%s%s", string(v), result)
-	}
-	return
 }
